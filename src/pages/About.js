99997c995a42f64e37942e5d4591b18c549dd620
Thad Cole <thadjcole@gmail.com>
2020-10-01 21:38:24 -0400

    WIP filling out About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 
 function About() {
-  return(
-    <div>this is the about page</div>
-  )
+  return (
+    <Container>
+      <Row>
+        <Col xs={4}>
+          <Image src="https://pbs.twimg.com/profile_images/814979740935290881/yxYJg4Av_400x400.jpg"/>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default About;
